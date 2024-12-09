591426b6eaa5490a119ea25655001ac92b8dccf4
Mohammed shuhaib <shuhaib@MacBookAir.mshome.net>
2024-12-09 19:21:10 +0530

    fix: import

--- src/atoms/button/index.tsx
+++ src/atoms/button/index.tsx
@@ -1,5 +1,4 @@
-import styled from "styled-components"; 
-import { Button } from "@mui/material";
+import { Button, styled } from "@mui/material";
 
 const CustomeButton=styled(Button)`
 &.MuiButton-contained{
