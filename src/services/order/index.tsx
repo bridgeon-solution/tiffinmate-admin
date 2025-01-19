340534b5c72ccc3ad183a76d4f81de832e8f5948
shahana <tshahana2894@gmail.com>
2025-01-19 16:23:07 +0530

    feature: change mock data

--- src/services/order/index.tsx
+++ src/services/order/index.tsx
@@ -67,6 +67,70 @@ export const GetSubscrition = async (page:number,selectedValue:number,search:str
       }
       return null;
     } catch {
-      toast.error("something went wrong")
+      toast.error("Error fetching foodItem")
+    }
+  };
+
+
+  export const GetAllOrders = async () => {
+    try {
+      
+      const response = await api.get(
+        '/Order'
+      );
+     
+      if (response && response.data && response.data.result) {
+        return response.data.result.totalCount;
+      }
+      return null;
+    } catch (error) {
+      toast.error("error fetching total orders")
+    }
+  };
+
+
+  export const GetTotalRevenue = async (totalOrder:number) => {
+    try {
+      
+      const response = await api.get(
+        `/Order?page=${1}&pageSize=${totalOrder}`
+      );
+     
+      if (response && response.data && response.data.result) {
+        const allDetails = response.data.result.allDetails;
+        const totalRevenue = allDetails.reduce((sum: number, order: { total_price: number }) => {
+          return sum + order.total_price
+        }, 0);
+        return totalRevenue;
+        
+        
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+
+  export const GetTotalRevenueSubscription = async () => {
+    try {
+      
+      const response = await api.get(
+        `/Subscription?page=${1}&pageSize=${10}`
+      );
+      if (response && response.data && response.data.result) {
+        const allDetails = response.data.result.allDetails;
+        const totalRevenue = allDetails.reduce((sum: number, order: { total_price: number }) => {
+          return sum + order.total_price
+          
+        }, 0);
+        return totalRevenue;
+
+        
+        
+      }
+      return null;
+    } catch (error) {
+      throw error;
     }
   };
